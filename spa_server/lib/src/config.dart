1b7c41ba5391a767f40f141633735318ef788af1
ironmask2004 <ironmask2004@gmail.com>
2021-09-13 20:35:52 +0300

    1

--- spa_server/lib/src/config.dart
+++ spa_server/lib/src/config.dart
@@ -2,11 +2,12 @@ import 'package:envify/envify.dart';
 
 part 'config.g.dart';
 
+
 @Envify()
 abstract class Env {
   static const secretKey = _Env.secretKey;
   static const mongoUrl = _Env.mongoUrl;
-  static const redisHost = '127.0.0.1';
-  static const   redisPort = 9999;
-  static const   serverPort = 9190;
+  static const redishost = _Env.redishost;
+  static const   redisport = _Env.redisport;
+  static const   serverport = _Env.serverport;
  }
