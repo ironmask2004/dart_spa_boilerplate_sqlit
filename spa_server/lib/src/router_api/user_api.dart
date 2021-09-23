9a4884dd417ad278ceab229d453b417386e2168e
ironmask2004 <ironmask2004@gmail.com>
2021-09-23 13:25:06 +0300

    add info to users

--- spa_server/lib/src/router_api/user_api.dart
+++ spa_server/lib/src/router_api/user_api.dart
@@ -17,6 +17,16 @@ class UserApi {
       });
     });
 
+    router.post('/', (Request req) async {
+      final authDetails = req.context['authDetails'] as JWT;
+      print('authDetails.subject.toString ' + authDetails.subject.toString());
+
+      User Curr_user = await User.findById( authDetails.subject.toString(), db);
+      print ("founded_user------:" + Curr_user.email!);
+      return Response.ok('{ "email": "${Curr_user.email}" }', headers: {
+        'content-type': 'application/json',
+      });
+    });
     final handler =
         Pipeline().addMiddleware(checkAuthorisation()).addHandler(router);
     return handler;
