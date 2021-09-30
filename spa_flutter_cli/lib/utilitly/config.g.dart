3fc77dcbfc38e0d1162bc06f2ed17bcb8db702a8
ironmask2004 <ironmask2004@gmail.com>
2021-09-30 14:59:30 +0300

    user cli login

---
+++ spa_flutter_cli/lib/utilitly/config.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'config.dart';
+
+// **************************************************************************
+// EnvifyGenerator
+// **************************************************************************
+
+class _Env {
+  static const serverHost = 'localhost';
+  static const serverPort = '9093';
+  static const baseUrl = '192.168.0.3:9093';
+}
